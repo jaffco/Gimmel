8a6776f3c715670c4d0b3b3fe0721992a027e1c8
Joel Jaffe <joel@jaffesd.com>
2025-01-17 09:18:17 -0800

    Revert "Implemented Pirkle Phaser with `Biquad` (Resolved Zero-Delay Feedback)"
    This reverts commit 2a75cc445b61d101a12cee9ae297640f2ff3a1c5.

--- include/phaser.hpp
+++ include/phaser.hpp
@@ -2,7 +2,7 @@
 #define GIML_PHASER_HPP
 #include "utility.hpp"
 #include "oscillator.hpp"
-#include "biquad.hpp"
+#include "filter.hpp"
 
 namespace giml {
     /**
@@ -10,24 +10,22 @@ namespace giml {
      * @tparam T floating-point type for input and output sample data such as `float`, `double`, or `long double`,
      * up to user what precision they are looking for (float is more performant)
      * @todo depth control (variable number of stages)
+     * @todo resolve zero-delay feedback
      */
     template <typename T>
     class Phaser : public Effect<T> {
     private:
         int sampleRate;
         size_t numStages = 0;
-        T rate = 0.0, feedback = 0.0;
+        T rate = 0.0, feedback = 0.0, last = 0.0;
         giml::TriOsc<T> osc;
-        std::vector<giml::Biquad<T>> filterbank;
+        giml::DynamicArray<giml::SVF<T>> filterbank;
 
     public:
         Phaser() = delete;
         Phaser(int samprate, size_t stages = 6) : sampleRate(samprate), numStages(stages), osc(samprate) {
             for (size_t stage = 0; stage < numStages; stage++) {
-                auto f = giml::Biquad<T>(samprate);
-                f.setType(Biquad<T>::BiquadUseCase::APF_2nd);
-                f.enable();
-                filterbank.push_back(f);
+                filterbank.pushBack(giml::SVF<T>());
             }
             this->setParams();
         }
@@ -38,35 +36,26 @@ namespace giml {
          * @return mix of current input and last output with time-varying comb filter
          */
         inline T processSample(const T& in) {
+            last = giml::powMix<T>(in, last, this->feedback);
+            T mod = osc.processSample();
 
-            // calculate feedback
-            T output = filterbank[numStages - 1].state();
-            T gamma = 1.0;
-            for (size_t stage = numStages - 2; stage < numStages; stage--) {
-                gamma *= filterbank[stage + 1].getG();
-                output += filterbank[stage].state() * gamma;
-            }
-            output *= -this->feedback;
-            output += in;
-            output /= 1 + this->feedback * gamma * filterbank[0].getG(); 
-
-            T mod = osc.processSample(); // get LFO value
             // pass through filterbank to create phase distortion
             for (size_t stage = 0; stage < numStages; stage++) {
-                T Fc = (this->sampleRate * 0.5) / (2.0 * (numStages - stage)); // should store these
-                this->filterbank[stage].setParams(Fc + mod * (Fc * 0.5));
-                output = this->filterbank[stage].processSample(output); // currently broken
+                T Fc = (this->sampleRate * 0.25) / (2.0 * (numStages - stage)); // should store these
+                this->filterbank[stage].setParams(Fc + mod * (Fc * 0.5), 2.0, sampleRate); // set cutoff frequency
+                this->filterbank[stage](last); // update filter state
+                last = this->filterbank[stage].allPass();
             }
 
-            output = giml::powMix<T>(in, output); // combine with input to create comb filter effect
+            last = giml::powMix<T>(in, last); // combine with input to create comb filter effect
             if (!this->enabled) { return in; }
-            return output; 
+            return last; 
         }
 
         /**
          * @brief sets rate, depth, feedback
          */
-        void setParams(T rate = 1.0, T feedback = 0.85) {
+        void setParams(const T& rate = 1.0, const T& feedback = 0.85) {
             this->setRate(rate);
             this->setFeedback(feedback);
         }
@@ -75,7 +64,7 @@ namespace giml {
          * @brief Set modulation rate- the frequency of the LFO.  
          * @param freq frequency in Hz 
          */
-        void setRate(T freq) {
+        void setRate(const T& freq) {
             this->osc.setFrequency(freq); // set frequency in Hz
         }
 
@@ -83,7 +72,7 @@ namespace giml {
          * @brief Set feedback gain. Clamped to [-1, 1].
          * @param fbGain feedback gain.
          */
-        void setFeedback(T fbGain) { 
+        void setFeedback(const T& fbGain) { 
             this->feedback = giml::clip<T>(fbGain, -1, 1); 
         }
     };
