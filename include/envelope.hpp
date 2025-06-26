7df22db10faf8b25537ea5637e81a9494454082b
Joel Jaffe <joel@jaffesd.com>
2025-06-25 18:29:46 -0700

    Prototyped `giml::EnvelopeFilter`

---
+++ include/envelope.hpp
@@ -0,0 +1,106 @@
+#ifndef GIML_ENVELOPE_HPP
+#define GIML_ENVELOPE_HPP
+
+#include "utility.hpp"
+#include "filter.hpp"
+
+namespace giml {
+    /**
+     * @brief Implements a basic envelope filter
+     */
+    template <typename T>
+    class EnvelopeFilter : public Effect<T> {
+    private:
+        int sampleRate;
+        T qFactor, aAttack, aRelease;
+        Vactrol<T> mVactrol;
+        SVF<T> mFilter;
+
+      
+    public:
+        // Constructor
+        EnvelopeFilter() = delete; // Do not allow an empty constructor, they must pass in a sampleRate
+        EnvelopeFilter(int sampleRate) : sampleRate(sampleRate), 
+                                         mVactrol(sampleRate), 
+                                         mFilter(sampleRate) {
+            this->setParams();
+        }
+        
+        // Destructor
+        ~EnvelopeFilter() {}
+
+        // Copy constructor
+        EnvelopeFilter(const EnvelopeFilter<T>& e) : 
+            sampleRate(e.sampleRate),
+            qFactor(e.qFactor),
+            aAttack(e.aAttack),
+            aRelease(e.aRelease),
+            mVactrol(e.mVactrol),
+            mFilter(e.mFilter)
+        {}
+
+        // Copy assignment operator 
+        EnvelopeFilter<T>& operator=(const EnvelopeFilter<T>& e) {
+            this->sampleRate = e.sampleRate;
+            this->qFactor = e.qFactor;
+            this->aAttack = e.aAttack;
+            this->aRelease = e.aRelease;
+            this->mVactrol = e.mVactrol;
+            this->mFilter = e.mFilter;
+            return *this;
+        }
+
+        inline T processSample(const float& in) override {
+            if (!this->enabled) { return in; }
+
+            // rectify, then smooth with vactrol
+            T rectfied = abs(in);
+            T cutoff = mVactrol(rectfied);
+
+            // warp cutoff and scale to desired frequency range
+            cutoff = std::log10f( (cutoff / 0.11111111111f) + 1.f );
+            cutoff = scale(cutoff, 0, 1, 185, 1900);
+            
+            // apply filter
+            mFilter.setParams(cutoff, qFactor, sampleRate);
+            mFilter(in);
+            return mFilter.loPass();
+        }        
+        
+        // Set parameters for the envelope filter
+        void setParams(T qFactor = 10.0, T attackMillis = 3.5, T releaseMillis = 100.0) {
+            this->setQ(qFactor);
+            this->setAttack(attackMillis);
+            this->setRelease(releaseMillis);
+        }
+
+        /**
+         * @brief set Q factor for the filter
+         * @param qFactor Q factor, floored at 1e-6
+         */
+        void setQ(T qFactor) {
+            qFactor = std::max(qFactor, T(1e-6));
+            this->qFactor = qFactor;
+        }
+
+        /**
+         * @brief set attack time 
+         * @param attackMillis attack time in milliseconds 
+         */
+        void setAttack(T attackMillis) { // calculated from Reiss et al. 2011 (Eq. 7)
+            this->aAttack = timeConstant(attackMillis, sampleRate);
+        }
+
+        /**
+         * @brief set release time 
+         * @param releaseMillis release time in milliseconds 
+         */
+        void setRelease(T releaseMillis) { // // 
+            this->aRelease = timeConstant(releaseMillis, sampleRate);
+        }
+
+    };
+
+} // namespace giml
+
+#endif
