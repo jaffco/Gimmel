a67d9ce293eb3212b2c47260a33a57ba03c98206
Joel A. Jaffe <joel@jaffesd.com>
2026-02-06 22:47:01 -0800

    Fix Biquad

--- include/biquad.hpp
+++ include/biquad.hpp
@@ -48,7 +48,6 @@ namespace giml {
 
         // Copy constructor
         Biquad(const Biquad<T>& b) {
-            this->enabled = b.enabled;
             this->useCase = b.useCase;
 
             this->sampleRate = b.sampleRate;
@@ -72,7 +71,6 @@ namespace giml {
 
         // Copy assignment operator
         Biquad<T>& operator=(const Biquad<T>& b) {
-            this->enabled = b.enabled;
             this->useCase = b.useCase;
 
             this->sampleRate = b.sampleRate;
@@ -215,10 +213,6 @@ namespace giml {
 
             prevY2 = prevY1;
             prevY1 = returnVal;
-
-            if (!(this->enabled)) {
-                return in;
-            }
 
             return returnVal;
         }
